6e59c9d26e872305d75acb7424458fcab6a6f4f2
Henry Story <henry.story@bblfish.net>
2021-04-04 11:38:17 +0200

    Isolate dependencies into seperate file

--- build.sbt
+++ build.sbt
@@ -1,12 +1,6 @@
-val Scala3Version = "3.0.0-RC2"
-val AkkaVersion = "2.6.13"
-val AkkaHttpVersion = "10.2.4"
-val circeVersion = "0.14.0-M4"
+import Dependencies._
 
-/**
- * [[https://www.scalatest.org/install home page]] and [[https://mvnrepository.com/artifact/org.scalatest/scalatest maven]]
- * */
-val scalatest = "org.scalatest" %% "scalatest" % "3.2.4" % "test"
+val Scala3Version = "3.0.0-RC2"
 
 lazy val root = project
 	.in(file("."))
@@ -18,62 +12,12 @@ lazy val root = project
 
 		scalaVersion := Scala3Version,
 
-		resolvers += Resolver.bintrayRepo("akka", "snapshots"),
+		//resolvers += Resolver.bintrayRepo("akka","snapshots"), //use if testing akka snapshots
+		resolvers += Resolver.sonatypeRepo("snapshots"), //for banana-rdf
 
-		libraryDependencies ++= Seq(
-			"com.typesafe.akka" %% "akka-actor-typed" % AkkaVersion,
-			"com.typesafe.akka" %% "akka-stream" % AkkaVersion,
-			"com.typesafe.akka" %% "akka-http" % AkkaHttpVersion,
-			"com.typesafe.akka" %% "akka-slf4j" % AkkaVersion,
-			"com.typesafe.akka" %% "akka-actor-testkit-typed" % AkkaVersion % Test,
-			"com.typesafe.akka" %% "akka-stream-testkit" % AkkaVersion % Test,
-			"com.typesafe.akka" %% "akka-http-testkit" % AkkaHttpVersion % Test,
-			// https://doc.akka.io/docs/alpakka/snapshot/
-			"com.lightbend.akka" %% "akka-stream-alpakka-file" % "2.0.2",
-			//http://logback.qos.ch/download.html
-			"ch.qos.logback" % "logback-classic" % "1.2.3",
-			"net.bblfish.rdf" %% "banana-rdf" % "0.8.5-SNAPSHOT",
-			"net.bblfish.rdf" %% "banana-jena" % "0.8.5-SNAPSHOT",
-			"org.tomitribe" % "tomitribe-http-signatures" % "1.7",
-			//"com.novocode" % "junit-interface" % "0.11" % "test"
-		).map(_.withDottyCompat(scalaVersion.value)),
+		libraryDependencies ++= dottyCompatLibs.map(_.withDottyCompat(scalaVersion.value)),
+		libraryDependencies ++= javaLibs,
+		libraryDependencies ++= scala3Libs,
 
-		// https://circe.github.io/circe/
-//		libraryDependencies ++= Seq(
-//			"io.circe" %% "circe-core",
-//			"io.circe" %% "circe-generic",
-//			"io.circe" %% "circe-parser"
-//		).map(_ % circeVersion),
-
-		//https://github.com/filip26/titanium-json-ld
-		//todo: this should be added to banana-rdf
-		libraryDependencies ++= Seq(
-			"com.apicatalog" % "titanium-json-ld" % "1.0.0",
-			// https://connect2id.com/products/nimbus-jose-jwt/examples/jwk-conversion
-			"com.nimbusds" % "nimbus-jose-jwt" % "9.7",
-			"org.glassfish" % "jakarta.json" % "2.0.0"
-		),
-
-		libraryDependencies ++= Seq(
-			//https://mvnrepository.com/artifact/org.typelevel/cats-core
-			"org.scalameta" %% "munit" % "0.7.23" % Test,
-//			"org.scalactic" %% "scalactic" % "3.2.5",
-			"org.scalatest" %% "scalatest" % "3.2.7" % Test,
-			"org.typelevel" %% "cats-core" % "2.5.0"
-		),
-
-		// java libs
-		libraryDependencies ++= Seq(
-			//https://mvnrepository.com/artifact/commons-codec/commons-codec
-			"commons-codec" % "commons-codec" % "1.15"
-		),
-		// https://mvnrepository.com/artifact/org.scalameta/munit
-		// https://scalameta.org/munit/docs/getting-started.html
 		testFrameworks += new TestFramework("munit.Framework")
-		//libraryDependencies += scalatest,
-
-		//https://mvnrepository.com/artifact/com.lihaoyi/utest
-		//https://github.com/lihaoyi/utest
-		//libraryDependencies += "com.lihaoyi" %% "utest" % "0.7.7" % "test",
-		// testFrameworks += new TestFramework("utest.runner.Framework")
 	)
---
+++ project/Dependencies.scala
@@ -0,0 +1,135 @@
+import sbt._
+import dotty.tools.sbtplugin.DottyPlugin.autoImport._
+import sbt.Keys.scalaVersion
+
+import scala.collection.immutable
+
+/**
+ * https://www.scala-sbt.org/1.x/docs/Organizing-Build.html
+ */
+object Dependencies {
+	val AkkaVersion = "2.6.13"
+	val AkkaHttpVersion = "10.2.4"
+	val scalazVersion = "7.4.0-M7"
+	val circeVersion = "0.14.0-M4"
+	val bananaVersion = "0.9.0-SNAPSHOT"
+	val alpakkaVersion = "2.0.2"
+
+	type NeedsDottyCompat = List[ModuleID]
+	val NeedsDottyCompat = List
+
+	//
+	// scala 2.13 libs
+
+	//
+	/**
+	 * Akka Http Core
+	 *
+	 * @see https://akka.io
+	 * @see https://repo1.maven.org/maven2/com/typesafe/akka
+	 * */
+	val akka = NeedsDottyCompat("com.typesafe.akka" %% "akka-actor-typed" % AkkaVersion,
+		"com.typesafe.akka" %% "akka-stream" % AkkaVersion,
+		"com.typesafe.akka" %% "akka-http" % AkkaHttpVersion,
+		"com.typesafe.akka" %% "akka-slf4j" % AkkaVersion)
+
+	val alpakka = "com.lightbend.akka" %% "akka-stream-alpakka-file" % alpakkaVersion
+
+	val akkaTest = NeedsDottyCompat("com.typesafe.akka" %% "akka-actor-testkit-typed" % AkkaVersion % Test,
+		"com.typesafe.akka" %% "akka-stream-testkit" % AkkaVersion % Test,
+		"com.typesafe.akka" %% "akka-http-testkit" % AkkaHttpVersion % Test)
+
+	/**
+	 * banana-rdf uses Scalaz so we won't use cats right now.
+	 * There is a 3.0.0 version of scalaz out, but until banana-rdf compiles to 3.0.0 we
+	 * need to use the 2.13 scala version
+	 * @see https://scalaz.github.io/7/
+	 */
+	val scalaz  = "org.scalaz" %% "scalaz-core" %  scalazVersion
+
+	/**
+	 * banana-rdf is still using 2.13
+	 * @see https://github.com/banana-rdf/banana-rdf
+	 */
+	val banana = NeedsDottyCompat(
+		"net.bblfish.rdf" %% "banana-rdf" % bananaVersion,
+		"net.bblfish.rdf" %% "banana-jena" % bananaVersion
+	)
+
+	def dottyCompatLibs: NeedsDottyCompat = akka ++ akkaTest ++ banana ++ Seq(scalaz, alpakka)
+
+	//
+	// Scala 3 libs
+	//
+
+	/**
+	 * [[https://www.scalatest.org/install home page]]
+	 * @see [[https://mvnrepository.com/artifact/org.scalatest/scalatest maven]]
+	 * */
+	val scalatest = "org.scalatest" %% "scalatest" % "3.2.7" % Test
+
+	/**
+	 * @see https://scalameta.org/munit/docs/getting-started.html
+	 * @see https://mvnrepository.com/artifact/org.scalameta/munit
+	 */
+	val munit = "org.scalameta" %% "munit" % "0.7.23" % Test
+
+	val scala3Libs = Seq(scalatest, munit)
+	//
+	// Java Libs
+	//
+
+	/**
+	 * Tomitribe HTTP Signatures implementation in Java
+	 * @see https://github.com/tomitribe/http-signatures-java
+	 */
+	val tomitribeHttpSig = "org.tomitribe" % "tomitribe-http-signatures" % "1.7"
+
+	/**
+	 * Titanium JSON-LD 1.1 parser. Only Java parser able to parse security vocab files it seems.
+	 * Should be integrated into banana-rdf.
+	 * @see https://github.com/filip26/titanium-json-ld
+	 */
+	val titaniumJSonLD = "com.apicatalog" % "titanium-json-ld" % "1.0.0"
+
+	/**
+	 * @see  https://connect2id.com/products/nimbus-jose-jwt/examples/jwk-conversion
+	 */
+	val nimbusDS = "com.nimbusds" % "nimbus-jose-jwt" % "9.7"
+
+	/**
+	 *  used by Akka logging
+	 *  @see http://logback.qos.ch/download.html
+	 */
+	val logback = "ch.qos.logback" % "logback-classic" % "1.2.3"
+
+	/**
+	 * @see https://mvnrepository.com/artifact/commons-codec/commons-codec
+	 */
+	val apacheCommonsCodec = "commons-codec" % "commons-codec" % "1.15"
+
+	val javaLibs = Seq(tomitribeHttpSig, titaniumJSonLD, nimbusDS, logback, apacheCommonsCodec)
+
+
+}
+
+//libraries that could be used later
+
+// https://circe.github.io/circe/
+//		libraryDependencies ++= Seq(
+//			"io.circe" %% "circe-core",
+//			"io.circe" %% "circe-generic",
+//			"io.circe" %% "circe-parser"
+//		).map(_ % circeVersion),
+
+//	// https://connect2id.com/products/nimbus-jose-jwt/examples/jwk-conversion
+//			"com.nimbusds" % "nimbus-jose-jwt" % "9.7",
+//			"org.glassfish" % "jakarta.json" % "2.0.0",
+
+////https://mvnrepository.com/artifact/org.typelevel/cats-core
+//				"org.typelevel" %% "cats-core" % "2.5.0"
+
+//https://mvnrepository.com/artifact/com.lihaoyi/utest
+//https://github.com/lihaoyi/utest
+//libraryDependencies += "com.lihaoyi" %% "utest" % "0.7.7" % "test",
+// testFrameworks += new TestFramework("utest.runner.Framework")
